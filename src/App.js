8fb184bd31e7249514a3418ab4fdd5efb6f53eb9
Guilherme Nono <Guilherme.nono@hotmail.com>
2022-03-29 13:59:00 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter , Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Login />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+
+const Header = (props) => { 
+    return (
+        <Nav>
+            <Logo>
+                <img src="/images/logo.svg" alt=""/>
+            </Logo>
+            <NavMenu>
+                <a href="/home">
+                    <img src="/images/home-icon.svg" alt=""/>
+                    <span>HOME</span>
+                </a>
+            </NavMenu>
+        </Nav>
+    )
+}
+
+const Nav = styled.nav`
+position: fixed;
+top: 0;
+left: 0;
+right: 0;
+height:75px;
+background-color: #090b13;
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 0 36px;
+letter-spacing: 16px;
+z-index: 3;
+`;
+
+const Logo = styled.a`
+padding: 0;
+width: 80px;
+margin-top: 4px;
+font-size:0;
+display: inline-block;
+img{
+    display: block;
+    width: 100%;
+}
+`;
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    justify-content: flex-end;
+    margin: 0px;
+    padding: 0px;
+    position: relative;
+    margin-right: auto;
+    margin-left: 25px;
+
+    a{
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img{
+            height: 20px;
+            min-width: 20px;
+            width: 20px;
+        }
+
+        span {
+            color: rgb(249, 249, 249);
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            line-height: 1.08;
+            padding: 2px 0;
+            white-space: nowrap;
+            position: relative;
+        }
+
+        &:before{
+            background-color: rgb(249, 249, 249);
+            border-radius: 0px 0px 4px 4px;
+        }
+    }
+
+    @media (max-width: 768px) {
+        display: none;
+     }
+`;
+
+export default Header;
